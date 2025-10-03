7bda20f86f086d19a3acb55ccf386f6a1a87acbc
Brijesh Gupta <brijeshngi@gmail.com>
2025-10-03 09:44:59 +0530

    added features

--- models/Firm.js
+++ models/Firm.js
@@ -16,7 +16,6 @@ const firmSchema = new mongoose.Schema(
       type: String,
       required: [true, "GSTIN is required"],
       unique: true,
-      match: [/^([0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1})$/, "Invalid GSTIN format"],
     },
   },
   { timestamps: true }
